b03b56de271fe1ecc03301fb3459a26e1d3e83b8
Mirsaid Akbaraliyev <akbaraliyev522@mail.ru>
2024-10-30 09:42:19 +0500

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import { BrowserRouter } from "react-router-dom";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <App />
+    <ToastContainer/>
+  </BrowserRouter>
+);
